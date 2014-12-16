7637fc944dff8a200475f8732eecf05e10333088
bmarek29 <marekbar29@gmail.com>
2014-12-16 13:23:08 +0100

    add connection to db

---
+++ src/kadrapplication/KadrManager.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kadrapplication;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author qqq
+ */
+public class KadrManager{
+    private Connection con;
+    private Statement st;
+    private ResultSet rs;
+    private PreparedStatement ps;
+    private final String url = "jdbc:mysql://localhost:3306/kadrapp";
+
+    private FillTable model;
+
+    public void getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, "root", "");
+            st = con.createStatement();
+            
+            System.out.println("połączono");
+        } catch (Exception e) {
+            System.out.println("sql error: "+e);
+        }
+    }
+    
+    public FillTable getPracownikData() {
+        ArrayList<Pracownik> pracowniks = new ArrayList<Pracownik>();
+        //Addon addon = new Addon();
+        try {
+            getConnection();
+            rs = st.executeQuery("select * from pracownik");
+            this.model = new FillTable(rs);
+
+        } catch (Exception e) {
+            System.out.println("bład " + e);
+        }
+        return model;
+    }
+}
